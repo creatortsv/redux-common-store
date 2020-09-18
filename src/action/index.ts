7c4e3bb708d0cd3bdcdb4ca7a574a2ef1ff1b876
creatortsv <creatortsv@yandex.ru>
2020-09-18 16:56:45 +0300

    Actions

--- src/action/index.ts
+++ src/action/index.ts
@@ -1,6 +1,8 @@
 import {
   CommonInstance,
-  Writable,
+  Updatable,
+  Compare,
+  Named,
 } from '..'
 
 export const SET_INSTANCES = 'SET_INSTANCES'
@@ -13,34 +15,51 @@ export interface CommonAction {
   [key: string]: unknown,
 }
 
-export interface NamedAction {
-  name: string,
-}
-
-export type ActionFn<T extends T[number][], R extends CommonAction = CommonAction> = (...payload: T) => R
-
-interface SetInstancesAction<T> extends NamedAction {
+interface SetInstancesAction<T> extends Named, CommonAction {
   type: typeof SET_INSTANCES,
   data: T[],
 }
 
-interface AddInstancesAction<T> extends NamedAction {
+interface AddInstancesAction<T> extends Named, CommonAction {
   type: typeof ADD_INSTANCES,
   data: T[],
 }
 
-interface UpdateInstancesAction<T> extends NamedAction {
+interface UpdateInstancesAction<T> extends Named, CommonAction {
   type: typeof UPD_INSTANCES,
   data: T[],
 }
 
-interface DeleteInstancesAction extends NamedAction {
+interface DeleteInstancesAction extends Named, CommonAction {
   type: typeof DEL_INSTANCES,
   identifiers: string[],
 }
 
-export type CommonActions<T extends CommonInstance> =
+export type CommonActions<T extends CommonInstance = CommonInstance> =
   SetInstancesAction<T> |
   AddInstancesAction<T> |
-  UpdateInstancesAction<Required<Pick<T, "id">> & Writable<Partial<T>>> |
-  DeleteInstancesAction 
+  UpdateInstancesAction<Updatable<T, 'id'>> |
+  DeleteInstancesAction
+
+export type Fn<T extends T[number][], R extends CommonAction = CommonAction> = (...payload: T) => R
+export type CreatorFn = <
+  T extends T[number][],
+  S extends CommonAction = Updatable<CommonActions<Compare<T[number], CommonInstance>>, 'type'>,
+>(type: S['type'], ...args: string[]) => Fn<T, S>
+
+export type NamedCreatorFn = <
+  T extends T[number][],
+  S extends CommonAction = Updatable<CommonActions<Compare<T[number], CommonInstance>>, 'type' | 'name'>,
+>(name: string, type: S['type'], ...args: string[]) => Fn<T, S>
+
+export function toObjects<T extends T[number][]>(payload: T, arg: string, i: number): T[number] {
+  return { [arg]: payload[i] }
+}
+
+export const makeNamedAction: NamedCreatorFn = (name, type, ...args) => (...payload) => Object
+  .assign({ type, name }, ...args
+    .map(toObjects.bind(null, payload)))
+
+export const makeAction: CreatorFn = (type, ...args) => (...payload) => Object
+  .assign({ type }, ...args
+    .map(toObjects.bind(null, payload)))
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 export interface Identifired {
-  id: string,
+  id: number | string,
+}
+
+export interface Named {
+  name: string,
 }
 
 export interface CommonInstance extends Readonly<Identifired> {}
+export type Updatable<T, K extends keyof T> = Pick<T, K> & Writable<Partial<T>>
 
 export type Writable<T> = {
   -readonly [K in keyof T]: T[K]
 }
+
+export type Unpack<T> = T extends (infer U)[] ? U : T;
+export type Compare<T, D> = Unpack<T> extends D ? Unpack<T> : D
